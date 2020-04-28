63bba9e1489664fc1ba0795e417ffc9f0ab9ab13
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-28 18:48:44 +0700

    Implemene Global Scope in Kotlin Coroutines

--- app/src/main/java/com/erikriosetiawan/globalscopecoroutines/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/globalscopecoroutines/MainActivity.kt
@@ -2,11 +2,25 @@ package com.erikriosetiawan.globalscopecoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            Log.d(TAG, "Coroutines says hello from THREAD ${Thread.currentThread().name}")
+        }
+
+        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 }
